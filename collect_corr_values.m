e60485510e49fdc0e4fcf3a4a86f4702048cc0f1
sju777 <suyeon.ju@yale.edu>
2022-07-08 17:22:36 -0400

    update cpm_output_stats to find medians of R's and p's, not mean

--- collect_corr_values.m
+++ collect_corr_values.m
@@ -1,5 +1,12 @@
 arr = [];
 for i = 1:7
-    arr = [arr; neon_by_sex_corrs.F.R.stdev.(char(f{i}))];
+    %% ravlt
+%     arr = [arr; ravlt_allsubjs_corrs.R_median.(char(scan_type_list{i})), ravlt_allsubjs_corrs.p_median.(char(scan_type_list{i}))];
+%     arr = [arr; ravlt_by_sex_corrs.F.R_median.(char(scan_type_list{i})), ravlt_by_sex_corrs.F.p_median.(char(scan_type_list{i}))];
+%     arr = [arr; ravlt_by_sex_corrs.M.R_median.(char(scan_type_list{i})), ravlt_by_sex_corrs.M.p_median.(char(scan_type_list{i}))];
+    %% neon
+%     arr = [arr; neon_allsubjs_corrs.R_median.(char(scan_type_list{i})), neon_allsubjs_corrs.p_median.(char(scan_type_list{i}))];
+%     arr = [arr; neon_by_sex_corrs.F.R_median.(char(scan_type_list{i})), neon_by_sex_corrs.F.p_median.(char(scan_type_list{i}))];
+%     arr = [arr; neon_by_sex_corrs.M.R_median.(char(scan_type_list{i})), neon_by_sex_corrs.M.p_median.(char(scan_type_list{i}))];
 end
 % disp(arr)
--- cpm_output_stats.m
+++ cpm_output_stats.m
@@ -8,10 +8,11 @@ function corrs = cpm_output_stats(scan_type_list, cpm_output_struct, allsubjs_or
 all_corrs = struct();
 if strcmp(allsubjs_or_sex, 'allsubjs') == 1
     for s = 1:length(scan_type_list)
-        all_corrs.('R').('avg').(char(scan_type_list{s})) = mean(cpm_output_struct.corr_struct.(char(scan_type_list{s}))(1,:));
-        all_corrs.('R').('stdev').(char(scan_type_list{s})) = std(cpm_output_struct.corr_struct.(char(scan_type_list{s}))(1,:));
-        all_corrs.('p').('avg').(char(scan_type_list{s})) = mean(cpm_output_struct.corr_struct.(char(scan_type_list{s}))(2,:));
-        all_corrs.('p').('stdev').(char(scan_type_list{s})) = std(cpm_output_struct.corr_struct.(char(scan_type_list{s}))(2,:));
+        corr_sorted = sort(cpm_output_struct.corr_struct.(char(scan_type_list{s}))(1,:));
+        median_R = corr_sorted(51);
+        all_corrs.('R_median').(char(scan_type_list{s})) = median_R;
+        all_corrs.('R_median_index').(char(scan_type_list{s})) = find(cpm_output_struct.corr_struct.(char(scan_type_list{s}))(1,:) == median_R);
+        all_corrs.('p_median').(char(scan_type_list{s})) = cpm_output_struct.corr_struct.(char(scan_type_list{s}))(2,all_corrs.('R_median_index').(char(scan_type_list{s})));
     end
     corrs = all_corrs;
 end
@@ -19,15 +20,17 @@ end
 all_corrs_by_sex = struct();
 if strcmp(allsubjs_or_sex, 'by_sex') == 1
     for s = 1:length(scan_type_list)
-        all_corrs_by_sex.('F').('R').('avg').(char(scan_type_list{s})) = mean(cpm_output_struct.('F').corr_struct.(char(scan_type_list{s}))(1,:));
-        all_corrs_by_sex.('F').('R').('stdev').(char(scan_type_list{s})) = std(cpm_output_struct.('F').corr_struct.(char(scan_type_list{s}))(1,:));
-        all_corrs_by_sex.('F').('p').('avg').(char(scan_type_list{s})) = mean(cpm_output_struct.('F').corr_struct.(char(scan_type_list{s}))(2,:));
-        all_corrs_by_sex.('F').('p').('stdev').(char(scan_type_list{s})) = std(cpm_output_struct.('F').corr_struct.(char(scan_type_list{s}))(2,:));
+        corr_sorted = sort(cpm_output_struct.F.corr_struct.(char(scan_type_list{s}))(1,:));
+        median_R_F = corr_sorted(51);
+        all_corrs_by_sex.('F').('R_median').(char(scan_type_list{s})) = median_R_F;
+        all_corrs_by_sex.('F').('R_median_index').(char(scan_type_list{s})) = find(cpm_output_struct.F.corr_struct.(char(scan_type_list{s}))(1,:) == median_R_F);
+        all_corrs_by_sex.('F').('p_median').(char(scan_type_list{s})) = cpm_output_struct.F.corr_struct.(char(scan_type_list{s}))(2,all_corrs_by_sex.F.('R_median_index').(char(scan_type_list{s})));
         
-        all_corrs_by_sex.('M').('R').('avg').(char(scan_type_list{s})) = mean(cpm_output_struct.('M').corr_struct.(char(scan_type_list{s}))(1,:));
-        all_corrs_by_sex.('M').('R').('stdev').(char(scan_type_list{s})) = std(cpm_output_struct.('M').corr_struct.(char(scan_type_list{s}))(1,:));
-        all_corrs_by_sex.('M').('p').('avg').(char(scan_type_list{s})) = mean(cpm_output_struct.('M').corr_struct.(char(scan_type_list{s}))(2,:));
-        all_corrs_by_sex.('M').('p').('stdev').(char(scan_type_list{s})) = std(cpm_output_struct.('M').corr_struct.(char(scan_type_list{s}))(2,:));
+        corr_sorted = sort(cpm_output_struct.('M').corr_struct.(char(scan_type_list{s}))(1,:));
+        median_R_M = corr_sorted(51);
+        all_corrs_by_sex.('M').('R_median').(char(scan_type_list{s})) = median_R_M;
+        all_corrs_by_sex.('M').('R_median_index').(char(scan_type_list{s})) = find(cpm_output_struct.M.corr_struct.(char(scan_type_list{s}))(1,:) == median_R_M);
+        all_corrs_by_sex.('M').('p_median').(char(scan_type_list{s})) = cpm_output_struct.M.corr_struct.(char(scan_type_list{s}))(2,all_corrs_by_sex.M.('R_median_index').(char(scan_type_list{s})));
     end
     corrs = all_corrs_by_sex;
 end
